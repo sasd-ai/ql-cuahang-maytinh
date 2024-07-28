c3cc89795f2013d989c16aed83f6108e43ff47fb
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-07-28 13:27:54 +0700

    Add Dao_ManHinh

---
+++ DAO/DAO_ManHinh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DAO
+{
+    public class DAO_ManHinh
+    {
+        private QL_CuaHangDataContext qlch=new QL_CuaHangDataContext();
+        public DAO_ManHinh() { }
+
+        public List<ManHinh> GetData()
+        {
+            return qlch.ManHinhs.Select(mh=>mh).ToList<ManHinh>();    
+        }
+    }
+}
