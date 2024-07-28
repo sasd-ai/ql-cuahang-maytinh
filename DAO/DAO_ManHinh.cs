6cebb04f33d95a1a77b6fca738bedc75aef669a2
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-07-28 13:33:36 +0700

    Add method Insert to DAO_ManHinh

--- DAO/DAO_ManHinh.cs
+++ DAO/DAO_ManHinh.cs
@@ -8,12 +8,33 @@ namespace DAO
 {
     public class DAO_ManHinh
     {
-        private QL_CuaHangDataContext qlch=new QL_CuaHangDataContext();
+        private QL_CuaHangDataContext qlch = new QL_CuaHangDataContext();
         public DAO_ManHinh() { }
 
         public List<ManHinh> GetData()
         {
-            return qlch.ManHinhs.Select(mh=>mh).ToList<ManHinh>();    
+            return qlch.ManHinhs.Select(mh => mh).ToList<ManHinh>();
+        }
+        public bool Insert(string maMH, string tenMH)
+        {
+            try
+            {
+                ManHinh mh = new ManHinh()
+                {
+                    MaManHinh = maMH,
+                    TenManHinh = tenMH
+                };
+                qlch.ManHinhs.InsertOnSubmit(mh);
+                qlch.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
         }
     }
 }
