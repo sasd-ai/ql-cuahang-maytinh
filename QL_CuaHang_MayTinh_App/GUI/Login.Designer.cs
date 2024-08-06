1b150d50b6942ea9950f5f66c3ee2139a03910bf
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-08-06 21:12:56 +0700

    Finals

--- QL_CuaHang_MayTinh_App/GUI/Login.Designer.cs
+++ QL_CuaHang_MayTinh_App/GUI/Login.Designer.cs
@@ -108,11 +108,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(724, 179);
+            this.label1.Location = new System.Drawing.Point(691, 160);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 38);
+            this.label1.Size = new System.Drawing.Size(283, 38);
             this.label1.TabIndex = 10;
-            this.label1.Text = "WELCOME";
+            this.label1.Text = "CỬA HÀNG TQĐ";
             // 
             // image
             // 
